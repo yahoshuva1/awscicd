ab45dcae8dd223b17e5cb6b1e3e8bfbc81083a34
yahoshuva1 <pyahoshuva@gmail.com>
2026-01-21 08:43:46 +0530

    initial commit

---
+++ src/main/java/com/aws/AwscicdApplication.java
@@ -0,0 +1,13 @@
+package com.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwscicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwscicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/AwscicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwscicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
